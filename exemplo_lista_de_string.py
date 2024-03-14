81c84290815f2df0fa10b9cc9eaa8379fd8ac2b9
Vitor Barbarisi <34862897+vitorbarbarisi@users.noreply.github.com>
2024-03-14 11:34:09 -0300

    Create exemplo_lista_de_string.py

---
+++ exemplo_lista_de_string.py
@@ -0,0 +1,13 @@
+frutas = ["maçã", "banana", "abacaxi"]
+print(f"Inicialmente a lista é: {frutas})
+
+#Vamos adicionar mais frutas:
+frutas.append("amora")
+frutas.append("morango")
+
+#Vamos retirar algumas frutas:
+frutas.remove("banana")
+frutas.remove("abacaxi")
+
+print(f"A lista final ficou assim: {frutas})
+
